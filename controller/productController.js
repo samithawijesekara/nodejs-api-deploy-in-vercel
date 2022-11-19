6ed36080e45bab9cc64b6268a56ae270a1796bc6
samithawijesekara <samithawijesekara@gmail.com>
2022-11-19 12:34:06 +0530

    Added env

--- controller/productController.js
+++ controller/productController.js
@@ -2,7 +2,7 @@ const getProductData = async(req, res) => {
     try {
         res.status(200).json({
             status: "200",
-            product_id: "795732957045",
+            product_id: process.env.PRODUCT_ID,
             name: "Sun wash cream"
         })
     } catch (error) {
